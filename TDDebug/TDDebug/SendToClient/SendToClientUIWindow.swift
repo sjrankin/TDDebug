7c8758f76ccc984ce2da170b50e10e36c2656670
Stuart Rankin <sjrankin@gmail.com>
2019-04-02 17:50:50 +0900

    Create SendToClientUIWindow.swift
    Code to run the Send to Client window.

---
+++ TDDebug/TDDebug/SendToClient/SendToClientUIWindow.swift
@@ -0,0 +1,22 @@
+//
+//  SendToClientUIWindow.swift
+//  TDDebug
+//
+//  Created by Stuart Rankin on 4/2/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class SendToClientUIWindow: NSWindowController
+{
+    weak var MainDelegate: MainProtocol? = nil
+        {
+     didSet
+     {
+        let VC = window!.contentViewController as? SendToClientUICode
+        VC?.Delegate = MainDelegate
+    }
+    }
+}
