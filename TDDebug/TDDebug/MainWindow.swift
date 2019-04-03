eb29dc0a8160f75a48b2a4a8e8c02ffa7b0aeed7
Stuart Rankin <sjrankin@gmail.com>
2019-04-03 13:24:10 +0900

    Create MainWindow.swift
    Added code to handle toolbar presses for the main window - these are merely passed along to the relevant view controller.

---
+++ TDDebug/TDDebug/MainWindow.swift
@@ -0,0 +1,41 @@
+//
+//  MainWindow.swift
+//  TDDebug
+//
+//  Created by Stuart Rankin on 4/3/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class MainWindow: NSWindowController
+{
+    @IBOutlet weak var DisconnectButton: NSToolbarItem!
+    @IBAction func HandleDisconnectButton(_ sender: Any)
+    {
+        let VC = window!.contentViewController as? ViewController
+        VC?.DoDisconnectFromClient()
+    }
+    
+    @IBOutlet weak var HelpButton: NSToolbarItem!
+    @IBAction func HandleHelpButtonPressed(_ sender: Any)
+    {
+        let VC = window!.contentViewController as? ViewController
+        VC?.DoShowHelp()
+    }
+    
+    @IBOutlet weak var SendButton: NSToolbarItem!
+    @IBAction func HandleSendToPeerButtonPressed(_ sender: Any)
+    {
+        let VC = window!.contentViewController as? ViewController
+        VC?.DoSendToClient()
+    }
+    
+    @IBOutlet weak var PeersButton: NSToolbarItem!
+    @IBAction func HandlePeersButtonPressed(_ sender: Any)
+    {
+        let VC = window!.contentViewController as? ViewController
+        VC?.DoShowPeers()
+    }
+}
