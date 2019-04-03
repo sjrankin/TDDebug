fe7e9ca888ad471b00d62830264e478bc43077c7
Stuart Rankin <sjrankin@gmail.com>
2019-04-03 16:07:34 +0900

    Create AboutTDDebugWindow.swift
    Window wrapper code around the About dialog box.

---
+++ TDDebug/TDDebug/About/AboutTDDebugWindow.swift
@@ -0,0 +1,15 @@
+//
+//  AboutTDDebugWindow.swift
+//  TDDebug
+//
+//  Created by Stuart Rankin on 4/3/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class AboutTTDebugWindow: NSWindowController
+{
+    
+}
