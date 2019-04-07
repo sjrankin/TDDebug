d2e7c35230cdf5b4fa76c131975874c6da3f8813
Stuart Rankin <sjrankin@gmail.com>
2019-04-07 22:09:22 +0900

    Update Versioning.swift
    Auto updated.

--- TDDebug/TDDebug/Versioning.swift
+++ TDDebug/TDDebug/Versioning.swift
@@ -55,19 +55,19 @@ public class Versioning
     }
     
     /// Build number.
-    public static let Build: Int = 430
+    public static let Build: Int = 443
     
     /// Build increment.
     private static let BuildIncrement = 1
     
     /// Build ID.
-    public static let BuildID: String = "B16D1B9E-BE71-4904-B6DA-2E891F000277"
+    public static let BuildID: String = "279DC74D-292C-4F57-9FBA-C86A7C74495A"
     
     /// Build date.
     public static let BuildDate: String = "7 April 2019"
     
     /// Build Time.
-    public static let BuildTime: String = "17:58"
+    public static let BuildTime: String = "22:07"
     
     /// Return a standard build string.
     ///
