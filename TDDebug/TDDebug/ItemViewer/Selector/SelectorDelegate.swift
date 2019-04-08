5d7d26ab6d27a6879e98afbf5875e2039cce6839
Stuart Rankin <sjrankin@gmail.com>
2019-04-08 14:25:04 +0900

    Create SelectorDelegate.swift
    Old code - will be removed soon.

---
+++ TDDebug/TDDebug/ItemViewer/Selector/SelectorDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SelectorDelegate.swift
+//  TDDebug
+//
+//  Created by Stuart Rankin on 4/8/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+protocol SelectorDelegate: class
+{
+    func SelectorTitle() -> String
+}
