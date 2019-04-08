2e97f7aa06b0b600e7b0593598aac0e7977d5b1e
Stuart Rankin <sjrankin@gmail.com>
2019-04-08 20:04:33 +0900

    Create CustomizeColorsFontsUICode.swift
    Placeholder code to drive UI for colors and fonts.

---
+++ TDDebug/TDDebug/Customization/CustomizeColorsFontsUICode.swift
@@ -0,0 +1,20 @@
+//
+//  CustomizeConnectionsUICode.swift
+//  TDDebug
+//
+//  Created by Stuart Rankin on 4/8/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class CustomizeColorsFontsUICode: NSViewController
+{
+    
+    
+    @IBAction func HandleDoneButton(_ sender: Any)
+    {
+        self.view.window!.performClose(sender)
+    }
+}
