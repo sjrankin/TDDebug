d741a62d9a206006a2d254ed14c914efd566f28d
Stuart Rankin <sjrankin@gmail.com>
2019-04-09 16:05:44 +0900

    Create LogItemProtocol.swift
    Protocol to provide a log item to those callers interested in it.

---
+++ TDDebug/TDDebug/LogItems/LogItemProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  LogItemProtocol.swift
+//  TDDebug
+//
+//  Created by Stuart Rankin on 4/9/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+
+protocol LogItemProtocol: class
+{
+    func LastSelectedLogItem() -> LogItem?
+}
