87d97e35b402e9e461e8b4f48994fa2a4794dd5c
Stuart Rankin <sjrankin@gmail.com>
2019-06-11 16:39:05 +0900

    Create PeerType.swift
    Added peer type class to encapsulate information on peer data.

---
+++ TDDebug/Common/MultiPeer/PeerType.swift
@@ -0,0 +1,48 @@
+//
+//  PeerType.swift
+//  TDDebug
+//
+//  Created by Stuart Rankin on 6/11/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+
+/// Will contain peer information.
+class PeerType
+{
+    /// Default initializer.
+    init()
+    {
+    }
+    
+    /// Holds the peer is a debugger flag.
+    private var _PeerIsDebugger: Bool = false
+    /// Get or set the peer is a debugger flag.
+    public var PeerIsDebugger: Bool
+    {
+        get
+        {
+            return _PeerIsDebugger
+        }
+        set
+        {
+            _PeerIsDebugger = newValue
+        }
+    }
+    
+    /// Holds the peer's prefix.
+    private var _PeerPrefixID: UUID? = nil
+    /// Get or set the peer's prefix value.
+    public var PeerPrefixID: UUID?
+    {
+        get
+        {
+            return _PeerPrefixID
+        }
+        set
+        {
+            _PeerPrefixID = newValue
+        }
+    }
+}
