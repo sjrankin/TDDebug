530973ba2bffcdccb4c3de42cc597991f83196dd
Stuart Rankin <sjrankin@gmail.com>
2019-06-24 21:59:18 +0900

    Create CommandDumpWindow.swift
    Placeholder code for the raw data dialog window.

---
+++ TDDebug/TDDebug/CommandDump/CommandDumpWindow.swift
@@ -0,0 +1,15 @@
+//
+//  CommandDumpWindow.swift
+//  TDDebug
+//
+//  Created by Stuart Rankin on 6/24/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class CommandDumpWindow: NSWindowController
+{
+    
+}
