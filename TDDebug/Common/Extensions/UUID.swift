a49fcdfea3c02269995ebcd92ad58df7aaaf44d4
Stuart Rankin <sjrankin@gmail.com>
2019-06-25 22:06:09 +0900

    Create UUID.swift
    Added Empty for UUID that returns an empty UUID.

---
+++ TDDebug/Common/Extensions/UUID.swift
@@ -0,0 +1,23 @@
+//
+//  UUID.swift
+//  TDDebug
+//
+//  Created by Stuart Rankin on 6/25/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+
+
+/// Convenience extensions for UUID.
+extension UUID
+{
+    /// Returns an empty UUID (all zero values for all fields).
+    static var Empty: UUID
+    {
+        get
+        {
+            return UUID(uuidString: "00000000-0000-0000-0000-000000000000")!
+        }
+    }
+}
