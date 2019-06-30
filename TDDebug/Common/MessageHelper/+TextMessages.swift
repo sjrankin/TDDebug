bf42ed58500b0c7969e095d8d132e58db2aef48a
Stuart Rankin <sjrankin@gmail.com>
2019-06-30 22:02:59 +0900

    Update +TextMessages.swift
    Added comments. Added capability to send a Marked flag.

--- TDDebug/Common/MessageHelper/+TextMessages.swift
+++ TDDebug/Common/MessageHelper/+TextMessages.swift
@@ -18,6 +18,12 @@ extension MessageHelper
 {
     // MARK: Text message command encoding commands.
     
+    /// Create a text message to send to a peer.
+    /// - Note: This function is probably obsolete and shouldn't be used anymore.
+    /// - Parameter WithType: The message type being sent.
+    /// - Parameter WithText: The text to send.
+    /// - Parameter HostName: The host name that sent the message.
+    /// - Returns: Properly formatted string to send.
     public static func MakeMessage(WithType: MessageTypes, _ WithText: String, _ HostName: String) -> String
     {
         let P1 = "Message=\(WithText)"
@@ -28,6 +34,10 @@ extension MessageHelper
         return Final
     }
     
+    /// Create a text message to send to a peer.
+    /// - Parameter WithText: The text to send.
+    /// - Parameter HostName: The host name that sent the message.
+    /// - Returns: Properly formatted string to send.
     public static func MakeMessage(_ WithText: String, _ HostName: String) -> String
     {
         let P1 = "Message=\(WithText)"
@@ -35,29 +45,52 @@ extension MessageHelper
         let P3 = "TimeStamp=\(MakeTimeStamp(FromDate: Date()))"
         let Final = GenerateCommand(Command: .TextMessage, Prefix: PrefixCode, Parts: [P1, P2, P3])
         return Final
-        
+    }
+    
+    /// Create a text message to send to a peer.
+    /// - Parameter WithText: The text to send.
+    /// - Parameter Marked: Context-dependent flag for the receipent to handle.
+    /// - Parameter HostName: The host name that sent the message.
+    /// - Returns: Properly formatted string to send.
+    public static func MakeMessage(_ WithText: String, Marked: Bool, _ HostName: String) -> String
+    {
+        let P1 = "Message=\(WithText)"
+        let P2 = "Marked=\(Marked)"
+        let P3 = "HostName=\(HostName)"
+        let P4 = "TimeStamp=\(MakeTimeStamp(FromDate: Date()))"
+        let Final = GenerateCommand(Command: .TextMessage, Prefix: PrefixCode, Parts: [P1, P2, P3, P4])
+        return Final
     }
     
     // MARK: Text message command decoding.
     
-    public static func DecodeTextMessage(_ Raw: String) -> (String, String, String)
-{
-    let Params = GetParameters(From: Raw, ["Message", "HostName", "TimeStamp"])
-    var Message = ""
-    if let Msg = Params["Message"]
+    /// Decodes a raw text command.
+    /// - Parameter Raw: The raw text command to parse.
+    /// - Returns: Tuple with parsed information. Contents of tuple are: The text message, the host name, the time stamp from
+    ///            the host, and the marked flag. (If the sender didn't use the Marked flag, this value will be false.)
+    public static func DecodeTextMessage(_ Raw: String) -> (String, String, String, Bool)
     {
-        Message = Msg
-    }
-    var HostName = ""
-    if let Host = Params["HostName"]
-    {
-        HostName = Host
-    }
-    var TimeStamp = ""
-    if let TS = Params["TimeStamp"]
-    {
-        TimeStamp = TS
-    }
-    return(Message, HostName, TimeStamp)
+        let Params = GetParameters(From: Raw, ["Message", "HostName", "TimeStamp", "Marked"])
+        var Message = ""
+        if let Msg = Params["Message"]
+        {
+            Message = Msg
+        }
+        var HostName = ""
+        if let Host = Params["HostName"]
+        {
+            HostName = Host
+        }
+        var TimeStamp = ""
+        if let TS = Params["TimeStamp"]
+        {
+            TimeStamp = TS
+        }
+        var Marked = false
+        if let Mk = Params["Marked"]
+        {
+            Marked = Bool(Mk)!
+        }
+        return(Message, HostName, TimeStamp, Marked)
     }
 }
